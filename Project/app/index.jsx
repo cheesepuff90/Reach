75dc1c712adab29ed330de8be555fad8026e04c8
Sooyoung-Park <sooyoung.park@vanderbilt.edu>
2024-04-12 23:21:12 -0500

    update

---
+++ Project/app/index.jsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Link } from 'expo-router'; // we need this to create nav link
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>index</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link> 
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});
