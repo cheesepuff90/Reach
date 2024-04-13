8a4683f652854dc68aca805926474688ab80daca
Sooyoung-Park <sooyoung.park@vanderbilt.edu>
2024-04-12 23:54:43 -0500

    asd

--- Project/app/index.jsx
+++ Project/app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router'; // we need this to create nav link
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>index</Text>
+      <Text className ="text-3xl font-pblack">index</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link> 
     </View>
